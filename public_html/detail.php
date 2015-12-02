5ea8b383a4ec3596250c8125b19a66677bee5493
Nina Sandlin <nina.sandlin@gmail.com>
2015-12-02 17:32:26 -0600

    Oops, coll-record-link stopped showing up; fixed

--- public_html/detail.php
+++ public_html/detail.php
@@ -62,7 +62,7 @@ $collrecd = "";
 if (!empty($record['RelRelatedMediaRef_tab'][0])) {
     if ($record['RelRelatedMediaRef_tab'][0]['MulMimeType'] == "x-url" && !empty($record['RelRelatedMediaRef_tab'][0]['MulIdentifier'])) {
         $collection_record_link = $record['RelRelatedMediaRef_tab'][0]['MulIdentifier'];
-        if ($collrecd) {
+        if ($collection_record_link) {
         $collrecd = ' <span class="view-collection-record"><a href="$collection_record_link" target="_blank">View collection record</a></span>';
         }
     }
