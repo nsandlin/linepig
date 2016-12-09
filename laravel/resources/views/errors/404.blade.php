6fc11bcb03d0931e9611d2bdfeba89a8587b5b0e
Pete Herbst <pherbst@fieldmuseum.org>
2016-12-09 10:39:53 -0600

    #33 initial commit of 404 page

---
+++ laravel/resources/views/errors/404.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>LinEpig - A resource for ID of female erigonines</title>
+  <meta name="description" content="A visual aid for identifying the difficult spiders in family Linyphiidae.">
+  <meta name="author" content="LinEpig, Field Museum of Natural History">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="http://fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <link rel="icon" type="image" href="images/favicon.ico">
+  <link rel="stylesheet" type="text/css" href="css/style-basic.css" />
+</head>
+<body>
+  <div class="container container-top">
+    <h1>Sorry - your image was not found</h1>
+    <p>One of these tools might help you locate it</p>
+    <p style="padding:0 0 0 40px;"> 
+       &bull; <a href="">Search LinEpig <i>[once this is ready]</i></a> <br><br>
+       &bull; <a href="http://linepig.fieldmuseum.org/">Visually browse the LineEpig main page:</a><br>
+       <a href="http://linepig.fieldmuseum.org/"><img src="images/mainpage.png"></a> <br><br>
+       &bull; <a href="http://www.fieldmuseum.org/science/special-projects/dwarf-spider-id-gallery">Refer to "About LineEpig"</a> <br><br>
+       &bull; <a href="http://www.wsc.nmbe.ch/family/48/Linyphiidae">Refer to "Linyphiidae" in the World Spider Catalog</a> <br> &nbsp;
+    </p>
+    <p style="padding:0 0 50px 0; border-bottom:dotted 1px blue;"> 
+       If you feel you have discovered a broken link or other problem on this site, please <a href="mailto:nsandlin@fieldmuseum.org">let us know</a>.
+    </p>
+
+    <p>LineEpig helps institutions identify the erigonines (family Linyphiidae) languishing in their collection.
+    <br>We have images for <b>296 species</b> so far. Read more <a href="https://www.fieldmuseum.org/science/special-projects/dwarf-spider-id-gallery" target="_blank">about LinEpig</a> and <a href="https://www.fieldmuseum.org/science/special-projects/dwarf-spider-id-gallery/can-you-help" target="_blank">help us grow</a>.</p>
+  </div><!-- container -->
+
+  <div id="bottomnav">
+    <a href="/index.php">LinEpig main page</a> - 
+    <a href="http://www.fieldmuseum.org/science/special-projects/dwarf-spider-id-gallery" target="_blank">About</a> - 
+    <a href="http://blogs.scientificamerican.com/guest-blog/internet-porn-fills-gap-in-spider-taxonomy/" target="_blank">Scientific American blog post</a> - 
+    <a href="http://www.fieldmuseum.org/science/special-projects/dwarf-spider-id-gallery/can-you-help" target="_blank">Contribute specimens/images</a> - 
+    <a href="https://github.com/nsandlin/linepig" target="_blank">GitHub</a>
+  </div><!--bottomnav-->
+  
+</body>
+</html>
