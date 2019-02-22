bc8c4da56ede131747441d8962ff60fcdd8173b6
Pete Herbst <pete@peteherbst.com>
2019-02-22 13:23:14 -0600

    Add Sneaker notifications

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -178,6 +178,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Custom Service Providers...
+         */
+        SquareBoat\Sneaker\SneakerServiceProvider::class
+
     ],
 
     /*
--- laravel/config/mail.php
+++ laravel/config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'itwebmailer@fieldmuseum.org',
+        'name' => 'LinEpig at The Field Museum',
     ],
 
     /*
