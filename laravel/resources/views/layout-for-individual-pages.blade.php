1ab07bff923f270b22a8795286b1423b53cdd322
Pete Herbst <pete@peteherbst.com>
2021-05-26 17:01:45 -0500

    Add new GA tag

--- laravel/resources/views/layout-for-individual-pages.blade.php
+++ laravel/resources/views/layout-for-individual-pages.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-88512602-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-88512602-1');
+  </script>
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -45,16 +55,5 @@
     <a href="https://github.com/nsandlin/linepig" target="_blank">GitHub</a><br>
     <p style="font-size:80%;">Cite: <b>LinEpig:</b> An ID Gallery for Female Erigoninae. Field Museum of Natural History, Chicago. Online at https://linepig.fieldmuseum.org/, accessed on {date of access}.</p> 
   </div><!--bottomnav-->
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-88512602-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
