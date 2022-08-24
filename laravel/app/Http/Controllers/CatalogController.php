44fd9a1502af54e23bc26a39009b3bda31d05c32
Pete Herbst <pete@peteherbst.com>
2022-08-24 09:28:26 -0500

    Update namespace for Models

--- laravel/app/Http/Controllers/CatalogController.php
+++ laravel/app/Http/Controllers/CatalogController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Catalog;
+use App\Models\Catalog;
 
 class CatalogController extends Controller
 {
--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Multimedia;
+use App\Models\Multimedia;
 
 class HomeController extends Controller
 {
