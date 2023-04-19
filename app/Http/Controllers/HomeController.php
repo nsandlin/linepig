ec0621a66f0c335e3dea698cd518d7d35bf43d21
Pete Herbst <pete@peteherbst.com>
2023-04-19 11:32:29 -0500

    Implement homepage pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Cache;
 use App\Models\Multimedia;
 
@@ -13,7 +14,7 @@ class HomeController extends Controller
      *
      * @return view
      */
-    public function showHome() 
+    public function showHome(Request $request) 
     {
         $records = Cache::remember('homepage_records', config('emuconfig.cache_ttl'), function () {
             $multimedia = new Multimedia();
@@ -36,13 +37,27 @@ class HomeController extends Controller
                 unset($records[$key]);
             }
         }
-
+        $records = collect($records);
         $count = count($records);
+
+        // Set up pagination
+        $perPage = 56;
+        $currentPage = $request->input('page', 1);
+        $paginator = new LengthAwarePaginator(
+            $records->forPage($currentPage, $perPage),
+            $count,
+            $perPage,
+            $currentPage,
+            [
+                'path' => $request->url(),
+                'query' => $request->query(),
+            ]
+        );
 
         $view = view('home', [
             'count' => $count,
-            'records' => $records,
-        ])->render();
+            'records' => $paginator,
+        ]);
 
         return $view;
     }
