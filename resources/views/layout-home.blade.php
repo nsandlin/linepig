c4be6952b979c014172f8b16a6f025566db9b1e0
Pete Herbst <pete@peteherbst.com>
2023-05-25 09:32:34 -0500

    Update site ID

--- resources/views/layout-home.blade.php
+++ resources/views/layout-home.blade.php
@@ -10,7 +10,7 @@
     (function() {
       var u="https://matomo.fieldmuseum.org/";
       _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '1']);
+      _paq.push(['setSiteId', '2']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
     })();
